26efe3a53c4f62464ff668d55fd57ac6f65c8947
Nicholas Zotalis <nzotalis@bu.edu>
2021-09-30 11:51:15 -0400

    read and tox

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,15 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, 'r') as file:
+        stuff = []
+        lines = file.readlines()
+        for line in lines:
+            r = []
+            for item in line.split(','):
+                try:
+                    r.append(float(item.strip()))
+                except:
+                    r.append(item.strip().replace('"', ''))
+            stuff.append(r)
+        return stuff
